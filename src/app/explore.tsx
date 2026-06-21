349d3595fec9c44a3e7275e450009394c3debf54
Adrian Garrido <adrian13ytg@hotmail.com>
2026-06-21 16:29:42 -0600

    Configurar proyecto Expo con NativeWind

---
+++ src/app/explore.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function ExploreScreen() {
+  return (
+    <View className="flex-1 items-center justify-center bg-slate-950">
+      <Text className="text-xl font-bold text-white">
+        Explore funcionando
+      </Text>
+    </View>
+  );
+}
---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View className="flex-1 items-center justify-center bg-slate-950">
+      <Text className="text-2xl font-bold text-white">
+        RestMenus funcionando
+      </Text>
+    </View>
+  );
+}
