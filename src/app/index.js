c90a56c1d9f9ad0675be735c0bf418bb089d9ee8
Adrian Garrido <adrian13ytg@hotmail.com>
2026-06-21 18:06:45 -0600

    Creacion de carpetas para organizar y una prueba

--- src/app/index.js
+++ src/app/index.js
@@ -1,11 +1,5 @@
-import { Text, View } from "react-native";
+import MenuScreen from "../components/menu/MenuScreen";
 
-export default function HomeScreen() {
-  return (
-    <View className="flex-1 items-center justify-center bg-slate-950">
-      <Text className="text-2xl font-bold text-white">
-        RestMenus funcionando
-      </Text>
-    </View>
-  );
+export default function Index() {
+  return <MenuScreen />;
 }
